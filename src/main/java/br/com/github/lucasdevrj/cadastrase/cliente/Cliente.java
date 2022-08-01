3d654af820df35199c3db5882f84e68061cff7cb
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-01 11:55:26 -0300

    Criação dos atributos da classe Cliente

--- src/main/java/br/com/github/lucasdevrj/cadastrase/cliente/Cliente.java
+++ src/main/java/br/com/github/lucasdevrj/cadastrase/cliente/Cliente.java
@@ -2,4 +2,19 @@ package br.com.github.lucasdevrj.cadastrase.cliente;
 
 public class Cliente {
 
+	private String nome;
+	private String sobrenome;
+	private String email;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
 }
