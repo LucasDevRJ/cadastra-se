7d6f9bd0fef8c53d9b28f20b9c46e1ff15c70d8f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-04 18:08:32 -0300

    Adição do método para validar dados

--- src/main/java/br/com/github/lucasdevrj/cadastrase/validacao/Validacao.java
+++ src/main/java/br/com/github/lucasdevrj/cadastrase/validacao/Validacao.java
@@ -1,6 +1,12 @@
 package br.com.github.lucasdevrj.cadastrase.validacao;
 
+import br.com.github.lucasdevrj.cadastrase.cliente.Cliente;
+
 public class Validacao {
 
-	
+	public void validaDados(Cliente cliente) {
+		if (cliente.getNome().isEmpty() || cliente.getSobrenome().isEmpty() || cliente.getEmail().isEmpty()) {
+			throw new NullPointerException("Insira todos os dados!");
+		}
+	}
 }
