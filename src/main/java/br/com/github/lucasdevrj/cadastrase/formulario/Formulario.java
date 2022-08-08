2adeab259df6d637f778936ff568fe96577ffed7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-08 18:43:42 -0300

    Alteração do tipo de método

--- src/main/java/br/com/github/lucasdevrj/cadastrase/formulario/Formulario.java
+++ src/main/java/br/com/github/lucasdevrj/cadastrase/formulario/Formulario.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.github.lucasdevrj.cadastrase.cliente.Cliente;
+import br.com.github.lucasdevrj.cadastrase.validacao.Validacao;
 
 @WebServlet("/formulario")
 public class Formulario extends HttpServlet {
@@ -18,16 +19,13 @@ public class Formulario extends HttpServlet {
 	Cliente cliente = new Cliente();
 	
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		cliente.setNome(req.getParameter("nome"));
 		cliente.setSobrenome(req.getParameter("sobrenome"));
 		cliente.setEmail(req.getParameter("email"));
 		
-		if (cliente.getNome().isEmpty() || cliente.getSobrenome().isEmpty() || cliente.getEmail().isEmpty()) {
-			throw new NullPointerException("Dados pessoais não informados!");
-		} else {
-			System.out.println("Cliente " + cliente.getNome() + " " + cliente.getSobrenome() + " está cadastrado com sucesso!");
-		}
+		Validacao valida = new Validacao();
+		valida.validaDados(cliente);
 		
 		PrintWriter escrever = resp.getWriter();
 		escrever.print("Olá " + cliente.getNome() + "! Muito obrigado.");
