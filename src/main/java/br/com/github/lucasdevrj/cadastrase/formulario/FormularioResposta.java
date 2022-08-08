3f60eda3fcdef38eeb9b58cd0f85ef2d64ff7914
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-08 18:51:44 -0300

    Criação de outra classe Servlet

---
+++ src/main/java/br/com/github/lucasdevrj/cadastrase/formulario/FormularioResposta.java
@@ -0,0 +1,17 @@
+package br.com.github.lucasdevrj.cadastrase.formulario;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FormularioResposta")
+public class FormularioResposta extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
