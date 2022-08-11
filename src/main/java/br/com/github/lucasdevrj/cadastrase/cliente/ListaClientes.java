47872c6ac45ad65c69860959d622990107f9bd33
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-08-11 20:44:45 -0300

    Remoção da escrita de jsp dentro do Java

--- src/main/java/br/com/github/lucasdevrj/cadastrase/cliente/ListaClientes.java
+++ src/main/java/br/com/github/lucasdevrj/cadastrase/cliente/ListaClientes.java
@@ -2,6 +2,7 @@ package br.com.github.lucasdevrj.cadastrase.cliente;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,27 +18,7 @@ public class ListaClientes extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Cadastro cadastro = new Cadastro();
+		List<Cliente> lista = cadastro.getClientes();
 		
-		PrintWriter escreve = response.getWriter();
-		
-		escreve.println("<html>");
-		escreve.println("<body>");
-		escreve.println("<ul>");
-		
-		for (int i = 0; i < cadastro.getClientes().size(); i++) {
-			String nome = cadastro.getClientes().get(i).getNome();
-			String sobrenome = cadastro.getClientes().get(i).getSobrenome();
-			String email = cadastro.getClientes().get(i).getEmail();
-			
-			escreve.println("<li>");
-			escreve.println("<p>Nome: " + nome + "</p>");
-			escreve.println("<p>Sobrenome: " + sobrenome + "</p>");
-			escreve.println("<p>E-mail: " + email + "</p>");
-			escreve.println("</li>");
-		}
-		
-		escreve.println("</ul>");
-		escreve.println("</body>");
-		escreve.println("</html>");
 	}
 }
